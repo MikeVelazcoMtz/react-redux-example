ebd84310260d87db0d4e433747f78500c600cd00
Miguel Angel Velazco Martinez <mvelazco@sparefoot.com>
2018-10-24 15:02:07 -0500

    Adding actions

---
+++ src/redux/actions.js
@@ -0,0 +1,18 @@
+
+// Types
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+// Other constants
+export const VilibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+};
+
+
+// Actions
+export const addTodo = (text) => ({type: ADD_TODO, text});
+export const toggleTodo = (index) => ({type: TOGGLE_TODO, index});
+export const setVisibilityFilter = (filter) => ({type: SET_VISIBILITY_FILTER, filter});
